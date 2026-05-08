550980e013ff97ef94fb22dba73ea0d867b58b9a
deltashopsiavash <siavashmamgadery09@gmail.com>
2026-05-08 12:53:01 +0430

    Initial delta version

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-docker ps -a --filter label=managed-by=mtproto-panel -q | xargs -r docker rm -f
-docker compose down
-read -rp "Remove database and .env too? [y/N]: " x
-[[ "$x" =~ ^[Yy]$ ]] && rm -rf data .env
